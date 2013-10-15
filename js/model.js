718769133599f56be0ae0f318e308b82227cd526
Anil Kumar <anilbms75@gmail.com>
2013-10-15 23:55:37 +0400

    memos for firefoxos using localStorage

--- js/model.js
+++ js/model.js
@@ -1,53 +1,6 @@
 /**
- * The memo app stores its data using indexedDB.
- *
- * This file holds the indexedDB routines and the routines for
- * working with the memos.
- *
- * First we handle the indexedDB setup then we implement the memo
- * handling routines.
+ * The memo app stores its data using localStorage.
  */
-
-var dbName = "memos";
-var dbVersion = 1;
-
-var db;
-var request = indexedDB.open(dbName, dbVersion);
-
-request.onerror = function (event) {
-    console.error("Can't open indexedDB!!!", event);
-};
-request.onsuccess = function (event) {
-    console.log("Database opened ok");
-    db = event.target.result;
-};
-
-request.onupgradeneeded = function (event) {
-
-    console.log("Running onUpgradeNeeded");
-
-    db = event.target.result;
-
-    if (!db.objectStoreNames.contains("memos")) {
-
-        console.log("Creating objectStore for memos");
-
-        var objectStore = db.createObjectStore("memos", {
-            keyPath: "id",
-            autoIncrement: true
-        });
-        objectStore.createIndex("title", "title", {
-            unique: false
-        });
-
-        console.log("Adding sample memo");
-        var sampleMemo1 = new Memo();
-        sampleMemo1.title = "Welcome Memo";
-        sampleMemo1.content = "This is a note taking app. Use the plus sign in the topleft corner of the main screen to add a new memo. Click a memo to edit it. All your changes are automatically saved.";
-
-        objectStore.add(sampleMemo1);
-    }
-};
 
 /**
  * This memo function is used to create new memos.
@@ -64,69 +17,81 @@ function Memo() {
 /**
  * This function will load all memos and fire its callback routine for each
  * memo loaded.
- * @param inCallback
+ * @public
+ * @param {Function} [inCallback]
  */
 function listAllMemoTitles(inCallback) {
-    var objectStore = db.transaction("memos").objectStore("memos");
-    console.log("Listing memos...");
+    var memosList = getMemos();
 
-    objectStore.openCursor().onsuccess = function (event) {
-        var cursor = event.target.result;
-        if (cursor) {
-            console.log("Found memo #" + cursor.value.id + " - " + cursor.value.title);
-            inCallback(null, cursor.value);
-            cursor.continue();
-        }
-    };
+    memosList.forEach(inCallback);
 }
 
 /**
- * This function is used to save a memo into the indexedDB database. It is called
- * on the 'change' event of the text inputs, so it is very aggressive. The idea behind
- * this is that the user never needs to save a memo for it is always in the saved state.
- * @param inMemo
- * @param inCallback
+ * Update localStorage
+ * @public
+ * @param {Object} [inMemo]
  */
-function saveMemo(inMemo, inCallback) {
-    var transaction = db.transaction(["memos"], "readwrite");
-    console.log("Saving memo");
+function saveMemo(inMemo) {
+    var memosList = getMemos();
 
-    transaction.oncomplete = function (event) {
-        console.log("All done");
-    };
-
-    transaction.onerror = function (event) {
-        console.error("Error saving memo:", event);
-        inCallback({
-            error: event
-        }, null);
-
-    };
-
-    var objectStore = transaction.objectStore("memos");
-
-    inMemo.modified = Date.now();
-
-    var request = objectStore.put(inMemo);
-    request.onsuccess = function (event) {
-        console.log("Memo saved with id: " + request.result);
-        inCallback(null, request.result);
-
-    };
+    // save inMemo in memosList array
+    if (memosList.length && isMemoPresent(memosList, inMemo.created) ) {
+        memosList.some(function (element) {
+                    if (element.created === inMemo.created) {
+                        element.title = inMemo.title;
+                        element.content = inMemo.content;
+                        element.modified = inMemo.modified;
+                    }
+            return element.created === inMemo.created;
+        });
+    }
+    else {
+        memosList.push(inMemo);
+    }
+    
+    // save to localStorage
+    localStorage.setItem("memosList", JSON.stringify(memosList));
 }
 
 /**
  * This function is used to remove a memo from the database. The only way to delete
  * a memo in this app is by using the trash button in the memo editing screen.
- * @param inId
- * @param inCallback
+ * @public
+ * @param {Number} [inId] Memo.created
  */
-function deleteMemo(inId, inCallback) {
-    console.log("Deleting memo...");
-    var request = db.transaction(["memos"], "readwrite").objectStore("memos").delete(inId);
+function deleteMemo(inId) {
+    var memosList = getMemos(); // get stored memoList
 
-    request.onsuccess = function (event) {
-        console.log("Memo deleted!");
-        inCallback();
-    };
+    //filtered memoList
+    var filteredList = memosList.filter(function (element) {
+        return element.created !== inId;
+    });
+
+    // localStorage update
+    localStorage.setItem("memosList", JSON.stringify(filteredList));
 }
+
+
+/**
+ * Get memosList
+ * @private
+ * @return [Array] memosList
+ */
+ function getMemos () {
+    return JSON.parse(localStorage.getItem("memosList")) || [];
+ }
+ /**
+  *checks if Memo present in the localStorage
+  *@private
+  *@param {Array} [Memoslist] Memos Array
+  *@param {Number} [Memo.created] Id to find match
+  *@return {Boolean} retruns true if present , false if its not present 
+  */
+ function isMemoPresent (memosList, createdId) {
+
+    var isMemoObject = memosList.some(function (element) {
+        return element.created === createdId;
+    });
+
+    return isMemoObject;
+ }
